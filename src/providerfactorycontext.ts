19fccc0a8a03e98f8189eb5747f6c0622007ba27
Frank Ballmeyer <Frank.Ballmeyer@assecosol.com>
2021-01-06 12:47:27 +0100

    New Version 6.0.0-beta3

--- src/providerfactorycontext.ts
+++ src/providerfactorycontext.ts
@@ -4,35 +4,35 @@ import { CrudItem, ValueType, EditLayout } from '@ballware/meta-interface';
 import { EditModes } from './editcontext';
 
 export interface ProviderFactoryContextState {
-    PageProviderFactory: (props: { identifier: string; children: JSX.Element | Array<JSX.Element> }) => JSX.Element;
-    MetaProviderFactory: (props: {
+    PageProvider: (props: { identifier: string; children: JSX.Element | Array<JSX.Element> }) => JSX.Element;
+    MetaProvider: (props: {
         entity: string;
         readOnly: boolean;
         headParams: Record<string, unknown>;
         initialCustomParam: Record<string, unknown>;
         children: JSX.Element | Array<JSX.Element>;
     }) => JSX.Element;
-    CrudProviderFactory: (props: {
+    CrudProvider: (props: {
         query: string | undefined;
         initialFetchParams: Record<string, unknown> | undefined;
         children: JSX.Element | Array<JSX.Element>;
     }) => JSX.Element;
-    LookupProviderFactory: (props: { children: JSX.Element | Array<JSX.Element> }) => JSX.Element;
-    NotificationProviderFactory: (props: { children: JSX.Element | Array<JSX.Element> }) => JSX.Element;
-    RightsProviderFactory: (props: {
+    LookupProvider: (props: { children: JSX.Element | Array<JSX.Element> }) => JSX.Element;
+    NotificationProvider: (props: { children: JSX.Element | Array<JSX.Element> }) => JSX.Element;
+    RightsProvider: (props: {
         client: string;
         secret: string;
         children: JSX.Element | Array<JSX.Element>;
     }) => JSX.Element;
-    TenantProviderFactory: (props: { children: JSX.Element | Array<JSX.Element> }) => JSX.Element;
-    EditProviderFactory: (props: {
+    TenantProvider: (props: { children: JSX.Element | Array<JSX.Element> }) => JSX.Element;
+    EditProvider: (props: {
         mode: EditModes;
         editLayout: EditLayout | undefined;
         item: CrudItem | Array<CrudItem> | ValueType;
         functionIdentifier?: string | undefined;
         children: JSX.Element | Array<JSX.Element>;
     }) => JSX.Element;
-    StatisticProviderFactory: (props: {
+    StatisticProvider: (props: {
         identifier: string;
         params: Record<string, unknown> | undefined;
         children: JSX.Element | Array<JSX.Element>;
